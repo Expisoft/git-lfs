e825be94d6471ed95b2a6dbdfcb3b6ebe7097045
Fredrik Simonsson <fs@expisoft.com>
2025-11-07 13:45:44 +0100

    Added certs_other.go with stub of Windows function

---
+++ lfshttp/certs_other.go
@@ -0,0 +1,9 @@
+// certs_other.go
+//go:build !windows
+
+package lfshttp
+
+// Just a stub on other os:es
+func getClientCertForHostFromSchannel(c *Client, host string) (*tls.Certificate, error) {
+
+}
